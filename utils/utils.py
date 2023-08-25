36181a91258efc09a62941bc8ceeec45470925eb
scy6500 <scy6500@kookmin.ac.kr>
2023-08-25 19:02:41 +0900

    feat: make dir if not exist

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import os
 import wave
 import librosa
 import configparser as parser
@@ -9,17 +10,22 @@ properties.read("config.ini")
 wav_detail = properties["WAV_DETAIL"]
 
 def bytes_to_wav(path, bytes_data, file_name):
-    with wave.open(path + "/" + file_name, 'wb') as wf:
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_path = path + "/" + file_name
+    with wave.open(file_path, 'wb') as wf:
         wf.setnchannels(int(wav_detail["num_channels"]))
         wf.setsampwidth(int(wav_detail["sample_width"]))
         wf.setframerate(int(wav_detail["sample_rate"]))
         wf.writeframes(bytes_data)
     wf.close()
+    audio_data, _ = librosa.load(file_path)
+    return audio_data
 
 def denoise_wav(path):
     audio = AudioDeNoise(path + "/top_channel.wav")
-    audio = AudioDeNoise(path + "/bottom_channel.wav")
     audio.deNoise(path + "/top_channel.wav")
+    audio = AudioDeNoise(path + "/bottom_channel.wav")
     audio.deNoise(path + "/bottom_channel.wav")
 
     top_channel_audio, _ = librosa.load(path + "/top_channel.wav")
