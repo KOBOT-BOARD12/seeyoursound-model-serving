35525f2dd11889efe75736616710d98b072e5633
scy6500 <scy6500@kookmin.ac.kr>
2023-09-03 18:26:39 +0900

    feat: code formatting

--- app.py
+++ app.py
@@ -7,20 +7,32 @@ from os.path import os, join, dirname
 from dotenv import load_dotenv, find_dotenv
 
 from utils.utils import bytes_to_wav, reduce_noise_mfcc_up
-from utils.model_utils import get_audio_classification_class, get_keyword_similarity, get_audio_direction, get_oscvm_result
+from utils.model_utils import (
+    get_audio_classification_class,
+    get_keyword_similarity,
+    get_audio_direction,
+    get_oscvm_result,
+)
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(find_dotenv())
 
 app = FastAPI()
 
+
 async def get_model_inference(req):
-    top_channel, bottom_channel, uid = req["top_channel"], req["bottom_channel"], req["uid"]
+    top_channel, bottom_channel, uid = (
+        req["top_channel"],
+        req["bottom_channel"],
+        req["uid"],
+    )
     filtered_class = req["filtered_class"]
 
     data = {"keyword": "unknown"}
 
-    top_channel_data, bottom_channel_data = bytes(base64.b64decode(top_channel.encode('utf-8'))), bytes(base64.b64decode(bottom_channel.encode('utf-8')))
+    top_channel_data, bottom_channel_data = bytes(
+        base64.b64decode(top_channel.encode("utf-8"))
+    ), bytes(base64.b64decode(bottom_channel.encode("utf-8")))
     top_channel_audio = bytes_to_wav(uid, top_channel_data, "top_channel.wav")
     bottom_channel_audio = bytes_to_wav(uid, bottom_channel_data, "bottom_channel.wav")
 
@@ -41,7 +53,7 @@ async def get_model_inference(req):
         return
     elif class_prediction == 4:
         keyword_prediction, flag = get_keyword_similarity(uid, top_channel_audio)
-        if flag == 0: 
+        if flag == 0:
             return
         data["keyword"] = keyword_prediction
 
@@ -51,8 +63,10 @@ async def get_model_inference(req):
     microphone_distance = 0.16  # Distance between microphones in meters
     max_tau = microphone_distance / sound_speed
 
-    theta = get_audio_direction(bottom_channel_audio, top_channel_audio, fs=16000, max_tau=max_tau)
-    theta=int(theta)
+    theta = get_audio_direction(
+        bottom_channel_audio, top_channel_audio, fs=16000, max_tau=max_tau
+    )
+    theta = int(theta)
 
     if theta > 0:
         if theta > 45:
@@ -66,7 +80,10 @@ async def get_model_inference(req):
             data["direction"] = "서쪽"
 
     async with httpx.AsyncClient() as client:
-        response = await client.post(os.getenv("SERVICE_SERVER_URL") + "/get_model_prediction", json=data)
+        response = await client.post(
+            os.getenv("SERVICE_SERVER_URL") + "/get_model_prediction", json=data
+        )
+
 
 @app.post("/prediction")
 async def return_prediction(audio_data: Request, background_tasks: BackgroundTasks):
